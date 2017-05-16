182c6ba2b2dda14ba370c976aa1df6c29fd60d4f
brian rush and nick wise <brian@>
2017-05-15 17:01:42 -0700

    add constructors for book objects, entry objects, and made a new book object to test our logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,34 @@
 //BUSINESS LOGIC BELOW
 
+//Library Object Constructor
+// function Library() {
+//   this.entriesArray = [];
+//   this.booksArray = [];
+// }
+//
+//Library Object to contain all books (and, therefore, entries)
+// var library = [bookOne, bookTwo];
+
 //Entry Object Constructor
-function Entry() {
-  this.bookTitle = "";
-  this.bookAuthor = "";
-  this.bookPage = 0;
-  this.entryTags = [];
-  this.imageId = "";
+function Entry(bookTitle, bookAuthor, bookPage, entryTags) {
+  this.bookTitle = bookTitle;
+  this.bookAuthor = bookAuthor;
+  this.bookPage = bookPage;
+  this.entryTags = entryTags;
+  // this.imageId = this.bookTitle + "-" + this.bookAuthor + imageNum;
 }
+
+var entryOne = new Entry("Lost Cat", "JASON", 0, ["bom bom","head", "wall"]);
+console.log(entryOne);
+alert(entryOne);
+
+// var entryTwo = new Entry() {
+//   bookTitle: "Lost Cat"
+//   bookAuthor: "JASON"
+//   bookPage: 0
+//   entryTags: ["fart","poop","wall"]
+//   imageId: this.bookTitle + "-" + this.bookAuthor + "-2"
+// }
 
 //Book Object Constructor
 function Book() {
@@ -15,23 +36,49 @@ function Book() {
   this.bookAuthor = "";
   this.commonEntryTags = [];
   this.includedImageIds = [];
+  this.entries = [];
 }
 
-//Make new entry
-var entryOne = new Entry()
-this.bookTitle = "Lost Cat";
-this.bookAuthor = "JASON";
-this.bookPage = 0;
-this.entryTags = ["BOM BOM","Head Bonk","Frustration", "Frustrated","Wall","Head","Impact", "Twice","Repeated"];
-this.imageId = "";
+// var bookOne = new Book() {
+//   this.bookTitle: "book one";
+//   this.bookAuthor: "tom";
+//   this.commonEntryTags: ["bang", "gun", "chicken"];
+//   this.includedImageIds: [];
+//   this.entries: [];
+// }
 
+//Make new entry
+// var entryOne = new Entry()
+// this.bookTitle = "Lost Cat";
+// this.bookAuthor = "JASON";
+// this.bookPage = 0;
+// this.entryTags = ["BOM BOM","Head Bonk","Frustration", "Frustrated","Wall","Head","Impact", "Twice","Repeated"];
+// this.imageId = "";
+
+// library.prototype.compareInputToTagArray(userInput) {
+//   // for (i = 0; i < this.entryTags.length; i++) {
+//     if (libraryOne.this.entryTags.includes(userInput)) {
+//       getElementsByClassName('userInput')
+//       // return userInput;
+//     }
+//   }
+// }
+
+
+library.forEach(function(book) {
+  // console.log(store.name + " sells:");
+  book.entries.forEach(function(product) {
+    console.log(entries.entryTags);
+  });
+  console.log("\n");
+});
 
 
 
 //USER LOGIC BELOW
 $(document).ready(function(){
-  $("#search").submit(function(event){
+  $("form#tag-search-form").submit(function(event){
     event.preventDefault();
-
+    var userInput = $("input#tag-search").val();
   });
 });
